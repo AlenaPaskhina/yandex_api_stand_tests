ee848b0398e579d086e54b8fb22b482b02a1fa77
alena_paskhina <alyona.paskhina@yandex.ru>
2023-11-13 11:06:04 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = " https://8a4c5aa1-a51a-41f1-8165-3c3c2079ff9d.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/" # создание пользователя
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ data.py
@@ -0,0 +1,13 @@
+headers = {
+    "Content-Type": "application/json" # заголовки и тело запроса
+}
+
+user_body = {
+    "firstName": "Анатолий",
+    "phone": "+79995553322",
+    "address": "г. Москва, ул. Пушкина, д. 10"
+}
+
+product_ids = {
+    "ids": [1, 2, 3]
+} #В файле data.py создай словарь product_ids
